484a31f512f4c8d84180315956078195e8ac3e4f
Abdulrahman Tohamy <119906363+th3tommy@users.noreply.github.com>
2023-04-11 13:10:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,7 @@ from aiogram.utils import executor
 
 
 class OpenAIBot:
-    def __init__(self, api_key, api_host, model, telegram_bot_token):
-        self.api_key = api_key
-        self.api_host = api_host
+    def __init__(self, model, telegram_bot_token):
         self.model = model
         self.bot = Bot(token=telegram_bot_token)
         self.dp = Dispatcher(self.bot)
@@ -16,25 +14,24 @@ class OpenAIBot:
     async def handle_message(self, message: types.Message):
         message_text = message.text
         chat_id = message.chat.id
+        print(message.text)
         await self.bot.send_chat_action(chat_id, types.ChatActions.TYPING)
-
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                    "https://openai80.p.rapidapi.com/chat/completions",
+                    "https://api.openai.com/v1/chat/completions",
                     headers={
-                        "content-type": "application/json",
-                        "X-RapidAPI-Key": self.api_key,
-                        "X-RapidAPI-Host": self.api_host
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + 'Authorization_KEY',
                     },
                     json={
                         "model": self.model,
-                        "messages": [{
-                            "role": "user",
-                            "content": message_text
-                        }]
+                        "messages": [{"role": "assistant", "content": f"{message_text}"}],
+                        "temperature": 1,
+                        "stop": None,
                     }
             ) as response:
                 response_json = await response.json()
+                print(response_json)
                 response_text = response_json["choices"][0]["message"]["content"]
                 await self.bot.send_message(chat_id=chat_id, text=response_text, reply_to_message_id=message.message_id)
 
@@ -50,11 +47,8 @@ class OpenAIBot:
 
 if __name__ == "__main__":
     bot = OpenAIBot(
-        api_key="YOUR_RAPIDAPI_KEY_HERE",
-        api_host="openai80.p.rapidapi.com",
         model="gpt-3.5-turbo",
-        telegram_bot_token="YOUR_BOT_TOKEN_HERE"
+        telegram_bot_token="telegram_bot_token"
     )
     tracemalloc.start()
     bot.start()
-
